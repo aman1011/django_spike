9edf1211554d71cecfc33990d2b60313a7aad464
Aman Gaur <gaman@athenahealth.com>
2019-04-02 21:16:21 +0530

    fixed the dynaodb port and modified settings for pycharm

--- hogwarts/hogwarts/settings.py
+++ hogwarts/hogwarts/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'nimbus',
+    'nimbus.apps.NimbusConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # DynamoDB local endpoint.
-DYNAMO_ENDPOINT = "http://localhost:8080"
+DYNAMO_ENDPOINT = "http://localhost:8000"
